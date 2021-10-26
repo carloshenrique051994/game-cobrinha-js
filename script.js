88273e9e6c6b03426a097c9a68c7edc0f2920bc4
carloshenrique051994 <58374815+carloshenrique051994@users.noreply.github.com>
2021-10-26 18:54:51 -0300

    adicionando estilização

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function criarBG() {
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(0, 0, 16 * box, 16 * box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 function drawFood() {
@@ -35,19 +35,18 @@ function update(event) {
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
     if (event.keyCode == 40 && direction != "up") direction = "down";
-
 }
 
 function iniciarJogo() {
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if (snake[0].x > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     for (i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
-            alert('Game Over');
+            alert("Game Over :(");
         }
     }
     criarBG();
@@ -63,7 +62,7 @@ function iniciarJogo() {
     if (direction == "down") snakeY += box;
 
     if (snakeX != food.x || snakeY != food.y) {
-        snake.pop
+        snake.pop();
     } else {
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
